75140b16125f84242587f78cf24d79eb8c3bd3a3
softwaredevemmanuel <48366297+softwaredevemmanuel@users.noreply.github.com>
2023-07-05 17:24:18 +0100

    pysqlite3

--- EventApplication/settings.py
+++ EventApplication/settings.py
@@ -105,8 +105,8 @@ WSGI_APPLICATION = "EventApplication.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.pysqlite3",
+        "NAME": BASE_DIR / "db.pysqlite3",
     }
 }
 
